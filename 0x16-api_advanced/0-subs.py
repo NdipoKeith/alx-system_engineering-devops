b70f0e5b84e489b19d6f9a87b3e93be5f47711a6
NdipoKeith <ndipokeith@gmail.com>
2024-01-16 05:51:13 -0800

    0x16-api_advanced

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,27 +1,38 @@
 #!/usr/bin/python3
 """
-number of subscribers for a given subreddit
+Python script is designed to fetch the number of subscribers for a given
+ subreddit from Reddit’s API.
 """
 
+# Import get function which is used to send HTTP requests.
 from requests import get
 
 
 def number_of_subscribers(subreddit):
     """
-    function that queries the Reddit API and returns the number of subscribers
-    (not active users, total subscribers) for a given subreddit.
+    defines a function that takes a subreddit name as an argument.
+    If an invalid subreddit is given, the function should return 0.
     """
 
+    # checks if the input is None or not a string
     if subreddit is None or not isinstance(subreddit, str):
         return 0
+    #  sets the user agent for the HTTP request.
+    headers = {'User-agent': 'My browser'}
 
-    user_agent = {'User-agent': 'Google Chrome Version 81.0.4044.129'}
+    # formats the URL to fetch data about the subreddit.
     url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-    response = get(url, headers=user_agent)
-    results = response.json()
 
+    # sends a GET request to the URL with the specified user agent.
+    response = get(url, headers=headers)
+
+    # converts the response from the GET request into JSON format.
+    result = response.json()
+
+    # tries to return the number of subscribers from the JSON data.
     try:
-        return results.get('data').get('subscribers')
+        return result.get('data').get('subscribers')
 
+    # If there’s an error (like the subreddit doesn’t exist), it returns 0.
     except Exception:
         return 0
